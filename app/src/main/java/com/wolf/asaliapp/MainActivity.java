8f7af699bc7d70d6cf40c48a0a6a71fed1e09a73
Wolfof420Street <billodida420@gmail.com>
2019-08-20 11:41:28 +0300

    Designing Login Page

--- app/src/main/java/com/wolf/asaliapp/MainActivity.java
+++ app/src/main/java/com/wolf/asaliapp/MainActivity.java
@@ -2,15 +2,62 @@ package com.wolf.asaliapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.lang.reflect.Type;
+
+
 
 public class MainActivity extends AppCompatActivity {
+
+   private TextView signIn;
+    private EditText emailEditText;
+    private EditText passwordEditText;
+    private TextView signUp;
+    private FirebaseAuth firebaseAuth;
+    private FirebaseUser firebaseUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        firebaseAuth = FirebaseAuth.getInstance();
+        firebaseUser = firebaseAuth.getCurrentUser();
 
+        signIn = findViewById(R.id.signin);
+        emailEditText = findViewById(R.id.email);
+        passwordEditText = findViewById(R.id.password);
+        signUp = findViewById(R.id.create);
+
+        signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, SignUpActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        signIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(MainActivity.this, SellHoneyActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+
+
+    
     }
 }
